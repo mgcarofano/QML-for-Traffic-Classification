05f1d56086c3d47d337e32370b14cfb5a7f8f52c
mgcarofano <m.carofano@studenti.unina.it>
2026-08-24 20:39:50 +0200

    Fusione notebooks
    + aggiornata descrizione markdown in Mirage_FlowPic_Classification.ipynb
    + adattata classe MirageDataset con cambio di rappresentazione del DEVICE
    + aggiunte funzioni get_plot_epoch_ticks(), get_plot_metric_ticks(), draw_train_val_plot() per disegnare in modo coerente i grafici di loss/accuracy nelle fasi di train/val
    + aggiunta costante USE_FLOWPIC_DATASET
    + migliorata gestione dello split in Train, Val, Test
    + migliorata fase di setup iperparametri (optimizer, scheduler, loss function)
    + rimossa costante LOSS_REGISTRY
    + aggiornato model_name di output
    + aggiunti risultati 2026-08-24_16-04 (val_acc 3.38%) e 2026-08-24_18-05 (test_acc 68.80%)

--- src/constants.py
+++ src/constants.py
@@ -8,7 +8,10 @@
 #   ####################################################################    #
 #   Miscellaneous
 
-EXEC_MODE_TRAIN = True
+RANDOM_SEED = 2025
+""" Random seed for reproducibility. """
+
+EXEC_MODE_TRAIN = False
 """ Set this flag to choose between training a new model
 or running validation on an already saved model. \n
 - True  : Execute the training loop.
@@ -16,15 +19,17 @@ or running validation on an already saved model. \n
 This allows running "Run All" in the notebook and automatically selecting
 the appropriate branch without manually skipping cells. """
 
-USE_PRECOMPUTED_DATASET = True
-""" Whether to use a precomputed FlowPic dataset. \n
-- False  : Generate the dataset from raw data using the `traffic_converter` module.
-- True   : Load the dataset from a precomputed .npy file. """
+USE_FLOWPIC_DATASET = (True, True)
+""" A pair of boolean values indicating whether to use the FlowPic dataset
+and whether to use a precomputed version of it. \n
+- (False, _) : Load the dataset from raw data using the .pickle file.
+- (True, False) : Generate the FlowPic dataset from raw data using the `traffic_converter` module.
+- (True, True) : Load the FlowPic dataset from a precomputed .npz file. """
 
 OUTPUT_DIR = "../results"
 """ Directory where the model and training history will be saved. """
 
-SAVE_OUTPUT = True
+SAVE_OUTPUT = False
 """ Whether to save the output of the notebook. """
 
 #   ####################################################################    #
@@ -88,15 +93,15 @@ VAL_SIZE = 0.14
 TEST_SIZE = 0.15
 """ Proportion of the dataset to be used for testing. """
 
-BATCH_SIZE = 32
+BATCH_SIZE = 64
 """ Size of the mini-batches used during training. """
 
-USE_NEW_SIZE = False
+USE_NEW_SIZE = True
 """ Whether to limit the number of training samples to a new size. """
 
-NEW_TRAIN_SIZE = 30000
-""" Limit on the number of training samples to use, to reduce training time.
-	If the training set is larger than this, it will be randomly sampled down to this size. """
+NEW_DATASET_SIZE = 30000
+""" Limit on the number of samples to use, to reduce training time and memory usage.
+	If the dataset is larger than this, it will be randomly sampled down to this size. """
 
 #   ####################################################################    #
 #	Model selection
@@ -118,52 +123,68 @@ MODEL_REGISTRY = {
 
     # flowpic_models
     "FlowPicCNN": ("../models.flowpic_models", "FlowPicCNN"),
-    "AmpHybridFlowPicCNN": ("../models.flowpic_models", "AmpHybridFlowPicCNN"),
+    "ResNet": ("../models.flowpic_models", "ResNet"),
 }
 """ A dictionary mapping model names to their corresponding module and class names. """
 
-MODEL_TIMESTAMP_ID = "2026-07-02_11-56"
+MODEL_TIMESTAMP_ID = "2026-08-24_18-05"
 """ Timestamp identifier for the model, used for saving and loading. """
 
-MODEL_NAME = "10E_Focal_MinMax_TrafficCNN"
+MODEL_NAME = "15E_AdamW_OneCycleSched_WeightedCrossEntropy_ResNet"
 """ Name of the model, used for saving and loading. \n
 The name format is *"{epochs}E\_{loss}\_{preprocessing}\_{model}"*. """
 
 #   ####################################################################    #
 #   Training configuration
 
-LOSS_REGISTRY = {
-	"CrossEntropy" : {
-		"params" : {}
-	},
-	"WeightedCrossEntropy" : {
-		"params" : {}
-	},
-	"Focal" : {
-		"params" : {
-			# ALPHA : può essere "class_weights", "uniform" o "custom".
-			"ALPHA" : "class_weights",
-
-			# GAMMA : parametro di focalizzazione, tipicamente tra 1 e 5.
-			"GAMMA" : 2.0
-		}
-	}
-}
-
-RANDOM_SEED = 2025
-""" Random seed for reproducibility. """
-
-EPOCHS = 10
+EPOCHS = 30
 """ Number of training epochs. """
 
-LEARNING_RATE = 1e-3
+LEARNING_RATE = 1e-2
 """ Learning rate for the optimizer. """
 
 EARLY_STOPPING = True
 """ Whether to use early stopping during training. """
 
-PATIENCE = 10
+PATIENCE = 7
 """ Number of epochs to wait for improvement before stopping training. """
+
+#   ####################################################################    #
+#   Optimizer configuration
+
+WEIGHT_DECAY = 1e-4
+""" Weight decay (L2 penalty) for the optimizer. """
+
+EPSILON = 1e-2
+""" Epsilon value for the optimizer AdamW. """
+
+MOMENTUM = 0.9
+""" Momentum for the optimizer SGD. """
+
+#   ####################################################################    #
+#   Scheduler configuration
+
+MAX_LR = LEARNING_RATE*3
+""" Maximum learning rate for the OneCycleLR scheduler. """
+
+START_FACTOR = 0.1
+""" Starting factor for the learning rate scheduler. """
+
+STEP_SIZE = 5
+""" Step size for the StepLR scheduler. """
+
+STEPLR_GAMMA = 0.1
+""" Multiplicative factor of learning rate decay for the StepLR scheduler. """
+
+#   ####################################################################    #
+#   Loss configuration
+
+ALPHA = "class_weights"
+""" Alpha parameter for the Focal Loss.
+Can be "class_weights", "uniform" or "custom". """
+
+FOCAL_LOSS_GAMMA = 2.0
+""" Gamma parameter for the Focal Loss, typically between 1 and 5. """
 
 #   ####################################################################    #
 #   Network configuration
